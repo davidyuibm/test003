7ba8a53ef2bd0b723fa0e753973e5a7fcdb0fd8f
davidyuibm <davidyuibm@gmail.com>
2022-08-19 21:56:09 +0800

    Fix time

--- my003/src/my003/My003.java
+++ my003/src/my003/My003.java
@@ -6,7 +6,7 @@ public class My003 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World by David Yu");
-		System.out.print("Test Git on 8/19 :34");
+		System.out.print("Test Git on 8/19 09:55 PM");
 		
 		// What happen test test dfgsd
 	}
