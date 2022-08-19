2bd26b12bef4289650aa07c6344234af82fe4756
davidyuibm <65706081+davidyuibm@users.noreply.github.com>
2022-08-19 21:57:58 +0800

    Update My003.java

--- my003/src/my003/My003.java
+++ my003/src/my003/My003.java
@@ -7,7 +7,7 @@ public class My003 {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World by David Yu");
 		System.out.print("Test Git on 8/19 09:55 PM");
-		
+		System.out.print("Git push on 8/19 09:57 PM");
 		// What happen test test dfgsd
 	}
 
