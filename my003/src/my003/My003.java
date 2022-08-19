70a028744b59e64647d150a68b2aa865e9c8ee40
davidyuibm <davidyuibm@gmail.com>
2022-08-19 21:47:11 +0800

    First push

---
+++ my003/src/my003/My003.java
@@ -0,0 +1,14 @@
+package my003;
+
+
+public class My003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World by David Yu");
+		System.out.print("Test Git on 8/19 :34");
+		
+		// What happen test test dfgsd
+	}
+
+}
